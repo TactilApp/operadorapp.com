e5d77681bce840ea619cf0bcb97e02fb2dde7b85
Jose da Rocha <jose@tactilapp.com>
2013-05-22 22:08:10 +0200

    subida inicial

---
+++ js/operadorapp.js
@@ -0,0 +1,194 @@
+var api_url = 'http://www.operadorapp.com/api/v1';
+var listado_de_operadoras  = new Array();
+
+$(document).ready(function() {
+	$("#div_resultado_numero_telefono").css("display", "none");
+	$("#div_resultado_compania").css("display", "none");
+	$("#boton_anterior").css("display", "none");
+		 
+  $("#boton_siguiente").click(comprobarOperadora);
+  $("#boton_anterior").click(mostrarCamposIniciales);
+  cargarOperadoras();
+ 	reloadCaptcha();
+});
+
+function reloadCaptcha(){
+	api_url = "js/captcha.json";
+	$.getJSON(api_url, function(data) {
+		captcha_url = data['captcha_url'];
+		$('#captcha').html('<img src="' + captcha_url + '" alt="captcha" />');
+		$("#captcha_str").val('');
+	});
+}
+
+function cargarOperadoras() {
+	$.ajax({
+		type: "GET",
+		url: "js/companies-color.plist",
+		dataType: "xml",
+		success: function(plist) {
+			var companhia_en_proceso = false;
+			var cadenas_en_proceso = false;
+	
+			var nombre_de_la_companhia = '';
+			var color_inferior = '';
+			var color_superior = '';
+			
+			$(plist).find("key").each(function () {
+				
+				if(!companhia_en_proceso){
+					companhia_en_proceso = true;
+					cadenas_en_proceso = false;
+					
+					color_inferior = '';
+					color_superior = '';
+					
+					nombre_de_la_companhia =  $(this).text();
+				}else{
+					var etiqueta = $(this).text();
+					var posicion = $(this).index();
+					
+					if(etiqueta == 'bottom'){
+						color_inferior =  $(this).parent().find("string").eq(posicion/2).text();
+					}else if(etiqueta == 'top'){
+						color_superior =  $(this).parent().find("string").eq(posicion/2).text();
+						var listado = $(this).parent().find("array");
+						if(listado.length == 0){
+							companhia_en_proceso = false;
+							finalizarCompanhia(nombre_de_la_companhia, color_inferior, color_superior, null);
+						}							
+					}else if(etiqueta == 'strings'){
+						var cadenas = new Array();
+						cadenas_en_proceso = true;
+						var numero_de_cadenas = 0;
+						$(this).parent().find("array").first().find("string").each(function () {
+								cadenas[numero_de_cadenas] = $(this).text();
+								numero_de_cadenas =  numero_de_cadenas + 1;
+						});
+						companhia_en_proceso = false;
+						finalizarCompanhia(nombre_de_la_companhia, color_inferior, color_superior, cadenas);
+					}
+				}
+			});
+
+		 /** for (i=0;i<listado_de_operadoras.length;i++){
+		  	alert('La compañía ' + listado_de_operadoras[i].nombre + ' tiene por arriba color ' + listado_de_operadoras[i].color_superior + ' y por abajo color ' + listado_de_operadoras[i].color_inferior);
+			}**/
+
+		}
+	});
+};
+
+function finalizarCompanhia(nombre_de_la_companhia, color_inferior, color_superior, cadenas){
+	listado_de_operadoras[listado_de_operadoras.length] = {nombre: nombre_de_la_companhia, color_superior: color_superior,  color_inferior: color_inferior};
+	if(cadenas != null && cadenas.length > 0){
+	  for (i=0;i<cadenas.length;i++){
+	  	listado_de_operadoras[listado_de_operadoras.length] = {nombre: cadenas[i], color_superior: color_superior,  color_inferior: color_inferior};
+		}
+	}
+}
+
+function buscarLaCompanhiaEnElXML(nombre_de_la_companhia){
+    return $.grep(listado_de_operadoras, function(item){
+      return item.nombre == nombre_de_la_companhia;
+    });
+};
+
+function comprobarOperadora() {
+	telefono = $("#mobile").val();
+	hay_telefono = false;
+	if(!telefono){
+		alert('Debe rellenar el número de teléfono para poder continuar.');
+	}else{
+		hay_telefono = true;
+	}
+
+	captcha = $("#captcha_str").val();
+	hay_captcha = false;
+	if(!captcha){
+		alert('Debe rellenar el captcha para poder continuar.');
+	}else{
+		hay_captcha = true;
+	}
+	
+	if(hay_telefono && hay_captcha){
+		api_url = 'js/companhia.json';
+		var resultado = "MASMOVIL";	
+
+		$.post(api_url, {
+			"captcha_str" : captcha,
+			"mobile" : telefono
+		}, function(data){
+			if (data.errors){
+				resultado = null;
+				alert(data.errors);
+			}else{
+				resultado = data.result.company;
+			}
+			reloadCaptcha();
+		}, "json");
+		
+		mostrarResultado(telefono, resultado);
+	}
+};
+
+function mostrarCamposIniciales() {		
+	$("#div_resultado_numero_telefono").css("display", "none");
+	$("#div_resultado_compania").css("display", "none");
+	$("#boton_anterior").css("display", "none");
+
+	$("#mobile").val("");
+	$("#div_numero_telefono").css("display", "block");
+	
+	$("#captcha_str").val("");
+	$("#div_captcha").css("display", "block");
+	
+	$("#boton_siguiente").css("display", "block");
+	
+	$("html, body").animate({ scrollTop: 0 }, "slow");
+};
+
+function mostrarResultado(telefono, resultado) {
+	$("#div_numero_telefono").css("display", "none");
+	$("#div_captcha").css("display", "none");
+	$("#boton_siguiente").css("display", "none");
+	
+	$("#div_resultado_numero_telefono > input").val(telefono);
+	$("#div_resultado_numero_telefono").css("display", "block");
+	
+	$("#company").val(resultado);
+
+
+	var color_superior = "868382";
+	var color_inferior = "d0cccb";
+
+	var companhia = buscarLaCompanhiaEnElXML(resultado)[0];
+	if(companhia==null){
+		companhia = buscarLaCompanhiaEnElXML("Default")[0];
+	}		
+	if(companhia != null){
+		color_superior = companhia.color_superior;
+		color_inferior = companhia.color_inferior;
+	}
+
+	var degradado_webkit = "-webkit-linear-gradient(top, #"+ color_superior +" 50%, #"+ color_inferior +" 50%)";
+	var degradado_webkit_base = "-webkit-gradient(linear, left top, left bottom, color-stop(0.5, #"+ color_superior +" ),color-stop(0.5, #"+ color_inferior +" ))";
+	var degradado_ms = "-ms-linear-gradient(top, #"+ color_superior +" 50%, #"+ color_inferior +" 50%)";
+	var degradado_o = "-o-linear-gradient(top, #"+ color_superior +" 50%, #"+ color_inferior +" 50%)";
+	var degradado_mozilla    = "-moz-linear-gradient(top, #" + color_superior + " 50%, #"+ color_inferior + " 50%)";
+	var degradado_base = "linear-gradient(top, #"+ color_superior +" 50%, #"+ color_inferior +" 50%)";
+	
+	$('#company')
+		.css({ background: degradado_webkit })
+		.css({ background: degradado_webkit_base })
+		.css({ background: degradado_ms })
+		.css({ background: degradado_o })
+		.css({ background: degradado_mozilla })
+		.css({ background: degradado_base });
+	
+	$("#div_resultado_compania").css("display", "block");
+
+	$("#boton_anterior").css("display", "block");
+	
+	$("html, body").animate({ scrollTop: 0 }, "slow");
+};
